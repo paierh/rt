75d5e1352d3064f1c9f1a7980e65fee1ac490a3d
Shawn M Moore <shawn@bestpractical.com>
2017-08-15 20:08:02 +0000

    List Database as source of configuration on Sys Config page

--- lib/RT/Config.pm
+++ lib/RT/Config.pm
@@ -2142,7 +2142,7 @@ sub SetFromConfig {
     }
 
     $META{$name}->{'Type'} = $type;
-    foreach (qw(Package File Line SiteConfig Extension)) {
+    foreach (qw(Package File Line SiteConfig Extension Database)) {
         $META{$name}->{'Source'}->{$_} = $args{$_};
     }
     $self->Set( $name, @{ $args{'Value'} } );
@@ -2392,6 +2392,7 @@ sub LoadConfigFromDatabase {
             Package    => 'N/A',
             File       => 'database',
             Line       => 'N/A',
+            Database   => 1,
             SiteConfig => 1,
         );
     }
